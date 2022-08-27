e13cef2c8d16b59159f591a223ffac4f11e0f789
Ivan Frolov <Frolls@outlook.com>
2022-08-27 13:12:21 +0500

    debug

--- vm-instance/outputs.tf
+++ vm-instance/outputs.tf
@@ -1,4 +1,4 @@
 output "yc_external_ipv4_address" {
-  value       = yandex_vpc_address.addr.external_ipv4_address[0].address
+  value       = "hellO!" #yandex_vpc_address.addr.external_ipv4_address[0].address
   description = "Внешний IP адрес"
 }
