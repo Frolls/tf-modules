8ce38550c62d0db6486b4760d55404382396b8eb
Ivan Frolov <Frolls@outlook.com>
2022-08-27 13:01:27 +0500

    add external IP address to output variable

--- vm-instance/outputs.tf
+++ vm-instance/outputs.tf
@@ -1,6 +1,4 @@
-
-/*output "yc_external_ipv4_address" {
-  value = yandex_vpc_address.addr.external_ipv4_address[0].address
+output "yc_external_ipv4_address" {
+  value       = yandex_vpc_address.addr.external_ipv4_address[0].address
+  description = "Внешний IP адрес"
 }
-
-*/
