ecfeefab9b17d59a05ad583da2f9ca3d72b6cca2
Ivan Frolov <Frolls@outlook.com>
2022-08-27 13:55:44 +0500

    bugfix

--- vm-instance/outputs.tf
+++ vm-instance/outputs.tf
@@ -1,9 +1,9 @@
 output "vm_external_ipv4_address" {
-  value       = yandex_compute_instance.vm.network_interface.0.nat_ip_addres
+  value       = yandex_compute_instance.vm.network_interface.0.nat_ip_address
   description = "Внешний IP адрес"
 }
 
 output "vm_internal_ipv4_address" {
-  value       = yandex_compute_instance.vm.network_interface.0.ip_addres
+  value       = yandex_compute_instance.vm.network_interface.0.ip_address
   description = "Внутренний IP адрес"
 }
