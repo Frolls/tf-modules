3ab627db56756368b5d98ba258b8de075513df74
Ivan Frolov <Frolls@outlook.com>
2022-08-29 20:17:33 +0500

    add vm_hostname output variable

--- vm-instance/outputs.tf
+++ vm-instance/outputs.tf
@@ -7,3 +7,8 @@ output "vm_internal_ipv4_address" {
   value       = yandex_compute_instance.vm.network_interface.0.ip_address
   description = "Внутренний IP адрес"
 }
+
+output "vm_hostname" {
+  value       = yandex_compute_instance.vm.hostname
+  description = "Имя хоста для инстанса"
+}
